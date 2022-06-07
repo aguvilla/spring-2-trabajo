1eaa5554a216719a34885c1c2bd5957460782953
agustin <agustinvillagran98@gmail.com>
2022-06-07 16:08:51 -0300

    post entrega

--- script.js
+++ script.js
@@ -3,37 +3,44 @@ const total = document.getElementById("total");
 let nombres = [];
 let montos = [];
 
-function obtenerdatos(){
+function obtenerdatos() {
     var nombre = document.getElementById('name').value;
     var monto = document.getElementById('monto').value;
-   if (nombre !="" && monto !=""){
-    nombres.push(nombre);
-    montos.push(monto);
-    } 
-    console.log(nombres)
-    console.log(montos);
+    if (nombre != "" && monto != "") {
+        nombres.push(nombre);
+        montos.push(monto);
+    }
+} 
+console.log(nombres);
+console.log(montos);
+
+function imprimirDatos(){
     let i = 0;
-    
     pGasto.innerHTML = "";
-      for (const dato of nombres) {  
+    for (const dato of nombres) {
         const p = document.createElement("p");
         p.className = "border border-3 border-secondary rounded p-1 mb-1 ps-3 fw-bold"
-        p.innerText = dato +": $"+ montos[i];
+        p.innerHTML = dato + ": $" + montos[i];
         pGasto.append(p);
-        i+=1;
+        i += 1;
     }
+}   
 
-    total.innerHTML ="";
+function totalGastos(){
+    total.innerHTML = "";
     let suma = 0;
-    for (const sum of montos){
+    for (const sum of montos) {
         suma += parseFloat(sum);
     }
     let division = suma / (nombres.length);
-    if (nombres.length == 0){
+    if (nombres.length == 0) {
         division = 0;
     }
-    total.innerHTML ="<p>Total: $" + suma +"</p>"+ "<p>A cada uno le toca aportar $:"+ division ;
-
-
+    total.innerHTML = "<p>Total: $" + suma + "</p>" + "<p>A cada uno le toca aportar $:" + division;
 }
 
+function boton() {
+    obtenerdatos();
+    imprimirDatos();
+    totalGastos();
+}
